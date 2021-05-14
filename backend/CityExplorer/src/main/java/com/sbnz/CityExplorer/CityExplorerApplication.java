78d5016ec0a3f2975f578e46eb9eab16e560cca0
Arpad Varga Šomođi <arpad.varga.somodji@gmail.com>
2021-05-14 16:16:44 +0200

    Finished KJAR integration

--- backend/CityExplorer/src/main/java/com/sbnz/CityExplorer/CityExplorerApplication.java
+++ backend/CityExplorer/src/main/java/com/sbnz/CityExplorer/CityExplorerApplication.java
@@ -2,32 +2,15 @@ package com.sbnz.CityExplorer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-
 
 @ComponentScan
 @SpringBootApplication
 public class CityExplorerApplication {
-	
+
 	public static void main(String[] args) {
-		//ApplicationContext ctx = 
 		SpringApplication.run(CityExplorerApplication.class, args);
 
-//		String[] beanNames = ctx.getBeanDefinitionNames();
-//		Arrays.sort(beanNames);
-//
-//		StringBuilder sb = new StringBuilder("Application beans:\n");
-//		for (String beanName : beanNames) {
-//			sb.append(beanName + "\n");
-//		}
-//		System.out.println(sb.toString());
-//		try {
-//			KieContainer container = KieContainerConfiguration.kieContainer();
-//		} catch (MavenInvocationException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
 	}
 
 }
--- backend/CityExplorer/src/main/java/com/sbnz/CityExplorer/service/IntegrationTest.java
+++ backend/CityExplorer/src/main/java/com/sbnz/CityExplorer/service/IntegrationTest.java
@@ -1,18 +1,6 @@
 package com.sbnz.CityExplorer.service;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Arrays;
-
-
-import org.apache.maven.shared.invoker.DefaultInvocationRequest;
-import org.apache.maven.shared.invoker.DefaultInvoker;
-import org.apache.maven.shared.invoker.InvocationRequest;
-import org.apache.maven.shared.invoker.Invoker;
 import org.apache.maven.shared.invoker.MavenInvocationException;
-
-import org.kie.api.KieServices;
-import org.kie.api.builder.KieScanner;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,40 +9,28 @@ import org.springframework.stereotype.Service;
 @Service
 public class IntegrationTest {
 
-//    @Autowired
+//  version 1, update after every request -- slower
+	
 //    private KieContainer kieContainer;
 //    private KieSession session;
 //    
-//    public void testRules() {
-//
+//    public void testRules() throws MavenInvocationException {
+//    	kieContainer = KieContainerConfiguration.kieContainer();
 //        session = kieContainer.newKieSession("ksession-rules");
 //        session.fireAllRules();
-//        session.destroy();
+//        session.dispose();
 //    }
-	
-	public void testRules() throws MavenInvocationException, IOException {
-		mavenCleanAndInstall();
-		KieServices ks = KieServices.Factory.get();
-		KieContainer kContainer = ks
-               .newKieContainer(ks.newReleaseId("com.sbnz", "CityExplorerKJAR", "1.0.0-SNAPSHOT"));
-		KieScanner kScanner = ks.newKieScanner(kContainer);
-		kScanner.start(6000);
-		KieSession session = kContainer.newKieSession("ksession-rules");
-		session.fireAllRules();
-		session.dispose();
-		System.out.println("testrules");
-	}
-	
-	public void mavenCleanAndInstall() throws MavenInvocationException, IOException {
-      InvocationRequest request = new DefaultInvocationRequest();
-      request.setPomFile(new File("../CityExplorerKJAR/pom.xml"));
-      request.setGoals(Arrays.asList("clean", "install"));
-      String currentPath = new File("../CityExplorerKJAR/pom.xml").getCanonicalPath();
-      System.out.println("Current dir:" + currentPath);
 
-      Invoker invoker = new DefaultInvoker();
-      invoker.setMavenHome(new File(System.getenv("MAVEN_HOME")));
-      invoker.execute(request);
-  }
+//  version 2, update at start and manually build kjar when update -- MUCH faster
+	
+    @Autowired
+    private KieContainer kieContainer;
+    private KieSession session;
+    
+    public void testRules(){
+        session = kieContainer.newKieSession("ksession-rules");
+        session.fireAllRules();
+        session.dispose();
+    }
 
 }
